8b89650739e93e02f88ae83922b54815b5dd8ad5
daniilSimakhin <danil.110@yandex.ru>
2022-08-12 21:28:14 +0700

    create extension for struct ToDoItem

--- ToDo/Controller/ViewController.swift
+++ ToDo/Controller/ViewController.swift
@@ -11,7 +11,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        var item = ToDoItem(text: "hello se", importance: .important, deadline: nil, isComplete: false, dateCreated: Date(), dateChanged: nil)
+        print(item.json)
+        let iitem = ToDoItem.parse(json: item.json)
+        print(iitem)
     }
 
 
--- ToDo/Model/ToDoItem.swift
+++ ToDo/Model/ToDoItem.swift
@@ -7,18 +7,77 @@
 
 import Foundation
 
-enum Importance {
+enum Importance: String {
     case important
     case unimportant
     case ordinary
+    
+    static func convertFromString(string: String) -> Importance {
+        if string == Importance.important.rawValue {
+            return Importance.important
+        } else if string == Importance.unimportant.rawValue {
+            return Importance.important
+        } else {
+            return Importance.important
+        }
+    }
 }
 
 struct ToDoItem {
     let id: String = UUID().uuidString
     let text: String
-    let importance: Importance = .ordinary
+    let importance: Importance
     let deadline: Date?
-    let isComplete: Bool = false
+    let isComplete: Bool
     let dateCreated: Date
     let dateChanged: Date?
+}
+
+extension ToDoItem {
+    var json: Any {
+        let nsDictionary = NSMutableDictionary(dictionary: [
+            "id": "\(id)",
+            "text": "\(text)",
+            "importance": "\(importance)",
+            "deadline": "\(deadline?.timeIntervalSince1970)",
+            "isComplete": "\(isComplete)",
+            "dateCreated": "\(dateCreated.timeIntervalSince1970)",
+            "dateChanged": "\(dateChanged?.timeIntervalSince1970)",
+        ])
+        if importance == .ordinary {
+            nsDictionary.removeObject(forKey: "importance")
+        }
+        if deadline == nil {
+            nsDictionary.removeObject(forKey: "deadline")
+        }
+        return nsDictionary
+    }
+    
+    static func parse(json: Any) -> ToDoItem? {
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+            let decoded = try JSONSerialization.jsonObject(with: jsonData, options: [])
+            if let dictFromJSON = decoded as? [String: String] {
+                print(dictFromJSON)
+                if let dateChangedFromJSON = dictFromJSON["dateChanged"], let deadlineFromJSON = dictFromJSON["deadline"] {
+                    return ToDoItem(text: dictFromJSON["text"]!,
+                                    importance: Importance.convertFromString(string: dictFromJSON["importance"]!),
+                                    deadline: Date(timeIntervalSince1970: TimeInterval(Float(deadlineFromJSON)!)),
+                                    isComplete: (dictFromJSON["isComplete"] != nil),
+                                    dateCreated: Date(timeIntervalSince1970:  TimeInterval(dictFromJSON["dateCreated"]!)!),
+                                    dateChanged: Date(timeIntervalSince1970: TimeInterval(Float(dateChangedFromJSON)!)))
+                } else {
+                    return ToDoItem(text: dictFromJSON["text"]!,
+                                    importance: Importance.convertFromString(string: dictFromJSON["importance"]!),
+                                    deadline: nil,
+                                    isComplete: (dictFromJSON["isComplete"] != nil),
+                                    dateCreated: Date(timeIntervalSince1970:  TimeInterval(dictFromJSON["dateCreated"]!)!),
+                                    dateChanged: nil)
+                }
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        return nil
+    }
 }
