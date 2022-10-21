c4d622177699e8075092f2204251909f2178b774
daniilSimakhin <danil.110@yandex.ru>
2022-10-21 16:01:15 +0700

    Add/Date-extension

--- ToDo/Supporting/Extensions/DateExtension.swift
+++ ToDo/Supporting/Extensions/DateExtension.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+extension Date {
+    static var tomorrow: Date {
+        let date = Calendar.current.date(bySettingHour: 12, minute: 0, second: 0, of: Date())!
+        return Calendar.current.date(byAdding: .day, value: 1, to: date)!
+    }
+}
